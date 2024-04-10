7e658aafff04b7a62d64e51bb78ae0f83e6876c1
ebielle <eryblongo@gmail.com>
2024-04-10 17:35:29 +0200

    finished exercise

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-/*Scrivere un programma che contenga una classe astratta chiamata Forma ed un
-metodo astratto chiamato calcolaArea(). Crea poi due sottoclassi Rettangolo e
-Triangolo che estendono Forma ed implementano il metodo per il calcolo dell'area
-in maniera specifica.*/
-
 public class Main {
 
     public static void main(String[] args) {
